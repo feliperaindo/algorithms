daee70ac599b9d94dad50a109e676156fd10e371
Felipe <felipe.raindo@gmail.com>
2023-08-22 01:38:17 -0300

    fix lint issues

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,32 +1,41 @@
 def check_time(time):
     return True if (type(time) is not int) else False
 
+
 def check_list(list_to_check):
     return True if type(list_to_check) is not list else False
 
+
 def check_tuple(tuple_to_check):
-    return True if type(tuple_to_check) is not tuple or len(tuple_to_check) != 2 else False
+    if type(tuple_to_check) is not tuple:
+        return True
+    if len(tuple_to_check) != 2:
+        return True
+    return False
+
 
 def check_tuple_values(tuple_to_check):
     for item in tuple_to_check:
-        if type(item) is not int: return True
+        if type(item) is not int:
+            return True
     return False
 
-def study_schedule(permanence_period, target_time):
-    if check_time(target_time): return None
-    if check_list(permanence_period): return None
-    
-    counter = 0
 
+def study_schedule(permanence_period, target_time):
+    if check_time(target_time) or check_list(permanence_period):
+        return None
+    counter = 0
     index = 0
     list_len = len(permanence_period)
     while index < list_len:
-        if check_tuple(permanence_period[index]): return None
-        if check_tuple_values(permanence_period[index]): return None
+        if (check_tuple(permanence_period[index])
+                or check_tuple_values(permanence_period[index])):
+            return None
 
-        first, second = permanence_period[index][0], permanence_period[index][1]
+        first, second = permanence_period[index]
 
-        if first <= target_time <= second: counter += 1
+        if first <= target_time <= second:
+            counter += 1
 
         index += 1
     return counter
