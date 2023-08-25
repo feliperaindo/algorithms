b5e13459d08399618ef1c1a5d4fe81b527bc5d81
Felipe <felipe.raindo@gmail.com>
2023-08-25 00:32:47 -0300

    solve req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or len(nums) < 2:
+        return False
+
+    dict_of_nums = {}
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+
+        if num in dict_of_nums:
+            return num
+        dict_of_nums[num] = 1
+    return False
